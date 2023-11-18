089b4a23c96a68c2971f0e8e43f4bd53a2bbc8be
charlieborin <148908691+charlieborin@users.noreply.github.com>
2023-11-18 19:44:03 +0000

    fix: :recycle: fix up comments

--- systems/x86_64-linux/funnybox/default.nix
+++ systems/x86_64-linux/funnybox/default.nix
@@ -7,8 +7,9 @@
 {
   imports =
     [
-      # Include the results of the hardware scan.
+      # Packages installed.
       ./packages.nix
+      # Include the results of the hardware scan.
       ./hardware.nix
     ];
   
@@ -147,23 +148,13 @@
   services.flatpak.enable = true;
   programs.virt-manager.enable = true;
 
-  # Steam
-
-  programs.steam.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   fanctl # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  # ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
