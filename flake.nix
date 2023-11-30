4d4d0c71d98002aa1ea87f0f1cf9d2cb5bcad753
charlieborin <148908691+charlieborin@users.noreply.github.com>
2023-11-30 02:10:26 +0000

    refactor: :recycle: switch to using Jack5079 modules
    I use a lot of code from my friend felt like a better idea to synchronise it

--- flake.nix
+++ flake.nix
@@ -48,11 +48,13 @@
       #
       systems.hosts.funnybox.modules = with inputs; [
         nix-flatpak.nixosModules.nix-flatpak
+        jack5079.nixosModules.hardened # Jack5079's NixOS hardened module
+        jack5079.nixosModules.virt-manager # Jack5079's Virt-Manager module
       ];
 
       # Home Manager modules
       homes.users."charlie@funnybox".modules = with inputs; [
-        jack5079.homeModules.bun
+        jack5079.homeModules.bun # Jack5079's bun module
       ];
     };
 }
