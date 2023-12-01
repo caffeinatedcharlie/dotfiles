70794d46a7ccab3ffb0f4ef1baa24d448af19033
charlieborin <148908691+charlieborin@users.noreply.github.com>
2023-12-01 18:44:11 +0000

    refactor: :art: MODULARISE!

--- systems/x86_64-linux/funnybox/default.nix
+++ systems/x86_64-linux/funnybox/default.nix
@@ -11,9 +11,13 @@
       ./hardware.nix
       # Desktop environment
       ./desktop.nix
+      # Enable sound
+      ./sound.nix
+      # Global services
+      ./services.nix
     ];
 
-  security.hardened = true;
+  security.hardened = true; # Enable NixOS hardening
 
   home-manager.useUserPackages = true; # "This is necessary if, for example, you wish to use `nixos-rebuild build-vm`" ―https://nix-community.github.io/home-manager/#:~:text=use%20nixos-rebuild-,build-vm,-.%20This%20option%20may
   home-manager.useGlobalPkgs = true;
@@ -23,6 +27,9 @@
 
   networking.hostName = "funnybox"; # Define your hostname.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+  time.timeZone = "Europe/London";
+  services.xserver.layout = "gb";
 
   nix = {
     # https://nixos.org/manual/nix/stable/command-ref/conf-file.html
@@ -46,51 +53,6 @@
     registry.nixpkgs.flake = inputs.nixpkgs; # https://github.com/NixOS/nix/pull/6530, "incompatible changes"
   };
 
-  # Set your time zone.
-  time.timeZone = "Europe/London";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # MULLVAD
-  services.mullvad-vpn = {
-    enable = true;
-    package = pkgs.mullvad-vpn;
-  };
-
-  # Configure keymap in X11
-  services.xserver.layout = "gb";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.charlie = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "libvirtd" "adbusers" ]; # Enable ‘sudo’ for the user.
@@ -98,32 +60,6 @@
     shell = pkgs.nushell;
   };
   environment.shells = with pkgs; [ nushell ];
-
-  programs.virt-manager.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
