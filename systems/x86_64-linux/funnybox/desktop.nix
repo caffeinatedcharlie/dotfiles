be17b075894c37df9c44e17279d8f33b10879260
charlieborin <148908691+charlieborin@users.noreply.github.com>
2023-12-01 17:48:55 +0000

    feat: :sparkles: INTER V4 IS IN NIXOS-UNSTABLE NOW YAY

--- systems/x86_64-linux/funnybox/desktop.nix
+++ systems/x86_64-linux/funnybox/desktop.nix
@@ -23,7 +23,7 @@
   ];
   # Fonts
   fonts = {
-    packages = [ inputs.nixpkgs-with-inter-v4.legacyPackages.${pkgs.system}.inter pkgs.noto-fonts-cjk-sans pkgs.source-sans-pro pkgs.go-font pkgs.monaspace ];
+    packages = [ pkgs.inter pkgs.noto-fonts-cjk-sans pkgs.source-sans-pro pkgs.go-font pkgs.monaspace ];
     fontDir.enable = true;
     fontconfig.defaultFonts = {
       sansSerif = [ "Inter" "Inter Regular" "Cantarell" "DejaVu Sans" ];
