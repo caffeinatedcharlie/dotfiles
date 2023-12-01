98eb0841f8ce409afccdf7824a5f641cb096ff76
charlieborin <148908691+charlieborin@users.noreply.github.com>
2023-12-01 16:28:31 +0000

    refactor: :fire: comment out flatpak code I plan to move it to a home manager module

--- systems/x86_64-linux/funnybox/flatpak.nix
+++ systems/x86_64-linux/funnybox/flatpak.nix
@@ -8,53 +8,53 @@
   # Enable flatpak support
   services.flatpak.enable = true;
 
-  # Custom fonts in Flatpak applications <https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1326957279>
-  system.fsPackages = [ pkgs.bindfs ];
-  fileSystems =
-    let
-      mkRoSymBind = path: {
-        device = path;
-        fsType = "fuse.bindfs";
-        options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
-      };
-      aggregatedFonts = pkgs.buildEnv {
-        name = "system-fonts";
-        paths = config.fonts.packages;
-        pathsToLink = [ "/share/fonts" ];
-      };
-    in
-    {
-      # Create an FHS mount to support flatpak host icons/fonts
-      "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
-      "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
-    };
+  # # Custom fonts in Flatpak applications <https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1326957279>
+  # system.fsPackages = [ pkgs.bindfs ];
+  # fileSystems =
+  #   let
+  #     mkRoSymBind = path: {
+  #       device = path;
+  #       fsType = "fuse.bindfs";
+  #       options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+  #     };
+  #     aggregatedFonts = pkgs.buildEnv {
+  #       name = "system-fonts";
+  #       paths = config.fonts.packages;
+  #       pathsToLink = [ "/share/fonts" ];
+  #     };
+  #   in
+  #   {
+  #     # Create an FHS mount to support flatpak host icons/fonts
+  #     "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
+  #     "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
+  #   };
 
-  # Flatpak declarative config
+  # # Flatpak declarative config
 
-  # services.flatpak.remotes = {
-  #   "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+  # # services.flatpak.remotes = {
+  # #   "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+  # # };
+
+  # # Updating flatpaks settings
+
+  # services.flatpak.update.onActivation = true;
+
+  # services.flatpak.update.auto = {
+  #   enable = true;
+  #   onCalendar = "weekly"; # Default value
   # };
 
-  # Updating flatpaks settings
-
-  services.flatpak.update.onActivation = true;
-
-  services.flatpak.update.auto = {
-    enable = true;
-    onCalendar = "weekly"; # Default value
-  };
-
-  services.flatpak.packages = [
-    "com.github.tchx84.Flatseal"
-    "com.github.micahflee.torbrowser-launcher"
-    "org.gnome.design.IconLibrary"
-    "org.gnome.design.Emblem"
-    "io.gitlab.adhami3310.Impression"
-    "de.haeckerfelix.Fragments"
-    "com.pojtinger.felicitas.Multiplex"
-    "io.mpv.Mpv"
-    "io.github.vinegarhq.Vinegar"
-    "info.febvre.Komikku"
-  ];
+  # services.flatpak.packages = [
+  #   "com.github.tchx84.Flatseal"
+  #   "com.github.micahflee.torbrowser-launcher"
+  #   "org.gnome.design.IconLibrary"
+  #   "org.gnome.design.Emblem"
+  #   "io.gitlab.adhami3310.Impression"
+  #   "de.haeckerfelix.Fragments"
+  #   "com.pojtinger.felicitas.Multiplex"
+  #   "io.mpv.Mpv"
+  #   "io.github.vinegarhq.Vinegar"
+  #   "info.febvre.Komikku"
+  # ];
 }
 
