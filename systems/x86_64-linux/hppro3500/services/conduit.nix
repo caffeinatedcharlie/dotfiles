0c2e0ab269ad48e1f7351d752702a29fc5cfaa4c
caffeinatedcharlie <git@downgraded.uk>
2024-01-08 18:52:06 +0000

    feat: :sparkles: HOST CONDUIT

---
+++ systems/x86_64-linux/hppro3500/services/conduit.nix
@@ -0,0 +1,31 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  services.matrix-conduit = {
+    enable = true;
+    settings.global = {
+      server_name = "downgraded.uk";
+      port = 6167;
+      database_backend = "rocksdb";
+      address = "127.0.0.1";
+    };
+  };
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "07e09863-d093-4aff-8f89-94f5f712c717" = {
+        credentialsFile = "/cloudflared/credentials/07e09863-d093-4aff-8f89-94f5f712c717.json";
+        default = "http_status:404";
+        ingress = {
+          "downgraded.uk" = with config.services.matrix-conduit.settings.global; {
+            service = "http://${address}:${toString port}";
+          };
+        };
+      };
+    };
+  };
+}
--- systems/x86_64-linux/hppro3500/services/hosting.nix
+++ systems/x86_64-linux/hppro3500/services/hosting.nix
@@ -8,5 +8,6 @@
   imports =
     [
       # ./gts.nix # GoToSocial (https://gts.busygam.ing)
+      ./conduit.nix # Rust conduit server
     ];
 }
