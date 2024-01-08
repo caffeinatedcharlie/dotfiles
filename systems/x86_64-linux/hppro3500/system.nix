08d4290638c4e33d88ca9e838030bedc86327b74
caffeinatedcharlie <git@downgraded.uk>
2024-01-08 18:27:36 +0000

    fix: :bug: add admin users to trusted store

--- systems/x86_64-linux/hppro3500/system.nix
+++ systems/x86_64-linux/hppro3500/system.nix
@@ -40,6 +40,10 @@
       use-xdg-base-directories = false; # Some bug makes $PATH not update to the new directories so for now I'm disabling this
       auto-optimise-store = true;
       log-lines = 10000000;
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
     };
     # https://github.com/Nyabinary/dotfiles/blob/4491af8ecc54fdd65ae4af7906080208682b15c9/hosts/default.nix#L30-L34
     gc = {
