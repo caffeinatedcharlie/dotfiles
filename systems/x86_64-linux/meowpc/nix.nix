052a6dded0f8504f2bd7ced2a4199e9562a9b24f
caffeinatedcharlie <git@downgraded.uk>
2024-02-28 00:46:10 +0000

    feat: :wrench: add Nix package manager configuration

---
+++ systems/x86_64-linux/meowpc/nix.nix
@@ -0,0 +1,32 @@
+{ inputs, ... }: {
+  nix = {
+    # https://nixos.org/manual/nix/stable/command-ref/conf-file.html
+    settings = {
+      # How to update this array later: Go to https://nixos.org/manual/nix/stable/contributing/experimental-features#currently-available-experimental-features
+      # and run `copy(JSON.stringify($$("#currently-available-experimental-features ~ h2").map(h2 => h2.textContent), null, 2).replaceAll(",", ""))`
+      experimental-features = [
+        "flakes"
+        "nix-command"
+      ];
+      use-xdg-base-directories = false; # Some bug makes $PATH not update to the new directories so for now I'm disabling this
+      auto-optimise-store = true;
+      log-lines = 10000000;
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
+    # https://github.com/Nyabinary/dotfiles/blob/4491af8ecc54fdd65ae4af7906080208682b15c9/hosts/default.nix#L30-L34
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    nixPath = [ "nixpkgs=flake:nixpkgs" ];
+  };
+}
