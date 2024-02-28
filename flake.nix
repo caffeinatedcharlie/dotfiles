953e8c84243d5f023878c5fd6714943d6c1fb91c
caffeinatedcharlie <git@downgraded.uk>
2024-02-28 00:08:36 +0000

    feat: :tada: add snowfall flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # We will handle this in the next section.
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      channels-config = {
+        allowUnfree = true;
+      };
+      namespace = "me";
+      package-namespace = "me";
+      snowfall.namespace = "me";
+      src = ./.;
+    };
+}
