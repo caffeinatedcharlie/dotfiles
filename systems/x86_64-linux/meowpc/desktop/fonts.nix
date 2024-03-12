cc6c639383765941c34fcf3828229546493a09a1
caffeinatedcharlie <git@downgraded.uk>
2024-03-12 18:45:02 +0000

    feat: :lipstick: add Monaspace nerd fonts

--- systems/x86_64-linux/meowpc/desktop/fonts.nix
+++ systems/x86_64-linux/meowpc/desktop/fonts.nix
@@ -2,11 +2,20 @@
 
 {
   fonts = {
-    packages = [ pkgs.inter pkgs.noto-fonts-cjk-sans pkgs.source-sans-pro pkgs.go-font pkgs.monaspace ];
+    packages = [
+      pkgs.inter
+      pkgs.noto-fonts-cjk-sans
+      pkgs.source-sans-pro
+      pkgs.go-font
+      pkgs.monaspace
+      (pkgs.nerdfonts.override {
+        fonts = [ "Monaspace" ];
+      })
+    ];
     fontDir.enable = true;
     fontconfig.defaultFonts = {
       sansSerif = [ "Inter Variable" "Inter Variable Regular" "Inter" "Inter Regular" "Cantarell" "DejaVu Sans" ];
-      monospace = [ "Monaspace Neon Var" "Monaspace Neon Regular" "DejaVu Sans Mono" ];
+      monospace = [ "Monaspice Nerd Font" "Monaspace Neon Var" "Monaspace Neon Regular" "DejaVu Sans Mono" ];
     };
   };
 }
