8f93512601fd899342ad103659ee0232289dbef1
caffeinatedcharlie <git@downgraded.uk>
2024-03-13 20:43:37 +0000

    refactor: :wrench: remove --print-build-logs
    dont exist wahhh

--- homes/x86_64-linux/charlie@meowpc/apps/terminal/nushell/default.nix
+++ homes/x86_64-linux/charlie@meowpc/apps/terminal/nushell/default.nix
@@ -2,7 +2,7 @@
   programs.nushell = {
     enable = true;
     shellAliases = {
-      switch = "sudo nixos-rebuild switch --fast --print-build-logs";
+      switch = "sudo nixos-rebuild switch --fast";
       nx = "nix develop --command hx";
       nv = "nix develop --command code";
       gc = "sudo nix-collect-garbage -d";
