c7aa92ad38b710b1e52d1f4eccbff20289833ccf
caffeinatedcharlie <git@downgraded.uk>
2024-03-15 23:07:27 +0000

    refactor: :lipstick: make catppuccin work

--- homes/x86_64-linux/charlie@meowpc/hyprland/qt.nix
+++ homes/x86_64-linux/charlie@meowpc/hyprland/qt.nix
@@ -6,15 +6,17 @@
     platformTheme = "qtct";
     style = {
       name = "kvantum";
-      package = pkgs.catppuccin-kvantum;
     };
   };
 
   xdg.configFile = {
     "Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini { }).generate "kvantum.kvconfig" {
-      General.Theme = "Catppuccin-Mocha-Mauve";
+      General.theme = "Catppuccin-Mocha-Mauve";
     };
 
-    "Kvantum/Catppuccin-Mocha-Mauve".source = "${pkgs.graphite-kde-theme}/share/Kvantum/Catppuccin-Mocha-Mauve";
+    "Kvantum/Catppuccin-Mocha-Mauve".source = "${pkgs.catppuccin-kvantum.override {
+      accent = "Mauve";
+      variant = "Mocha";
+    }}/share/Kvantum/Catppuccin-Mocha-Mauve";
   };
 }
