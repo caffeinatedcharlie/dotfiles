8bc50841a2e456f1e80b7b6ad1ebc4c0f8cfa197
caffeinatedcharlie <git@downgraded.uk>
2024-03-16 00:09:01 +0000

    ci(system): :hammer: add vm start shell script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eux
+CONFNAME=$1
+
+nix build .#nixosConfigurations.${CONFNAME}.config.system.build.vm
+# Horrible hack to force it to use the system QEMU to allow OpenGL to work. Please never ever do this.
+# sed "s/\/nix\/store\/.*qemu-kvm/qemu-system-x86_64/g" ./result/bin/run-${CONFNAME}-vm | bash
+./result/bin/run-${CONFNAME}-vm
