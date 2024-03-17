329c9ba0bd3d727edf406dec9e7bd2a669e9272c
caffeinatedcharlie <git@downgraded.uk>
2024-03-17 00:08:16 +0000

    refactor(shell): :construction_worker: remove the result and qcow2 before building the VM

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 set -eux
 CONFNAME=$1
 
+rm -rf ./result
+rm -rf ./*.qcow2
 nix build .#nixosConfigurations.${CONFNAME}.config.system.build.vm
 # Horrible hack to force it to use the system QEMU to allow OpenGL to work. Please never ever do this.
 # sed "s/\/nix\/store\/.*qemu-kvm/qemu-system-x86_64/g" ./result/bin/run-${CONFNAME}-vm | bash
