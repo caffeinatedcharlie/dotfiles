c7b509bc8bf2c9a51abc4fbb6b4eab7b55e394d7
caffeinatedcharlie <git@downgraded.uk>
2024-03-17 00:01:46 +0000

    fix(apps/terminal): :adhesive_bandage: fix the attributes

--- homes/x86_64-linux/charlie@meowpc/apps/terminal/git/default.nix
+++ homes/x86_64-linux/charlie@meowpc/apps/terminal/git/default.nix
@@ -1,17 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.git = {
     enable = true;
     userName = "caffeinatedcharlie";
     userEmail = "git@downgraded.uk";
+    attributes = [ "*.lockb binary diff=lockb" ];
     extraConfig = {
       core.editor = "code --wait";
       init.defaultBranch = "main";
       commit.gpgsign = true;
       user.signingkey = "~/.ssh/id_ed25519.pub";
       gpg.format = "ssh";
-      attributes = [ "*.lockb binary diff=lockb" ];
       diff.lockb = {
         textconv = lib.getExe pkgs.bun;
         binary = true;
