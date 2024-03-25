dfd77a808f796a679b6694933645f61b84798796
caffeinatedcharlie <git@downgraded.uk>
2024-03-25 08:08:54 +0000

    fix(shell): :bug: user not uid

--- homes/x86_64-linux/charlie@meowpc/apps/terminal/nushell/default.nix
+++ homes/x86_64-linux/charlie@meowpc/apps/terminal/nushell/default.nix
@@ -7,7 +7,7 @@
       nv = "nix develop --command code";
       gc = "sudo nix-collect-garbage -d";
       code = "codium";
-      mount_data = "sudo mount -o uid=1000 -t bcachefs /dev/disk/by-partuuid/fdc75711-559a-4908-9eda-5a912a6440b4:/dev/disk/by-partuuid/8209bc50-fb37-432c-b137-13d97ad65478:/dev/disk/by-partuuid/3ddfb494-022b-44c9-9f3e-e13c7985b847 /data";
+      mount_data = "sudo mount -o user=1000 -t bcachefs /dev/disk/by-partuuid/fdc75711-559a-4908-9eda-5a912a6440b4:/dev/disk/by-partuuid/8209bc50-fb37-432c-b137-13d97ad65478:/dev/disk/by-partuuid/3ddfb494-022b-44c9-9f3e-e13c7985b847 /home/charlie/data";
     };
     extraConfig = ''
       $env.config.show_banner = false;
